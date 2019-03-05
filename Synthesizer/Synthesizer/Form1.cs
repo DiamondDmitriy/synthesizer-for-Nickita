a1a41123a42ce7d79808fe1c7827c505b6e69514
DiamondDmitriy <47792118+DiamondDmitriy@users.noreply.github.com>
2019-03-05 19:15:19 +0300

    finish

--- Synthesizer/Synthesizer/Form1.cs
+++ Synthesizer/Synthesizer/Form1.cs
@@ -44,25 +44,117 @@ namespace Synthesizer
             {
 
                 case Keys.Z:
-                    frequency = 65.4f; // c2
+                    frequency = 220.0000f; 
                     break;
                 case Keys.X:
-                    frequency = 138.59f; //c3
+                    frequency = 246.9417f; 
                     break;
+                case Keys.S:
+                    frequency = 233.0819f; 
+                    break;
+
                 case Keys.C:
-                    frequency = 261.62f; //c4
+                    frequency = 261.6256f;
+                    break;
+                case Keys.F:
+                    frequency = 277.1826f; 
                     break;
                 case Keys.V:
-                    frequency = 523.25f; //c5
+                    frequency = 293.6648f;
+                    break;
+
+                case Keys.G:
+                    frequency = 311.1270f; 
                     break;
                 case Keys.B:
-                    frequency = 1046.5f; //c6
+                    frequency = 329.6276f; 
                     break;
                 case Keys.N:
-                    frequency = 2093f; //c7
+                    frequency = 349.2282f; 
+                    break;
+
+                case Keys.J:
+                    frequency = 369.9944f; 
                     break;
                 case Keys.M:
-                    frequency = 4186.01f; //c8
+                    frequency = 391.9954f; 
+                    break;
+                case Keys.K:
+                    frequency = 415.3047f;
+                    break;
+                case Keys.Oemcomma:
+                    frequency = 440.0000f; 
+                    break;
+
+
+                case Keys.L:
+                    frequency = 466.1638f; 
+                    break;
+                case Keys.OemPeriod:
+                    frequency = 493.8833f; 
+                    break;
+                case Keys.OemQuestion:
+                    frequency = 523.2511f; 
+                    break;
+
+                case Keys.D1:
+                    frequency = 554.3653f; 
+                    break;
+                case Keys.Q:
+                    frequency = 587.3295f; 
+                    break;
+                case Keys.D2:
+                    frequency = 622.2540f; 
+                    break;
+                case Keys.W:
+                    frequency = 659.2551f; 
+                    break;
+                case Keys.E:
+                    frequency = 698.4565f; 
+                    break;
+                case Keys.D3:
+                    frequency = 739.9888f; 
+                    break;
+                case Keys.R:
+                    frequency = 783.9909f; 
+                    break;
+
+                case Keys.D4:
+                    frequency = 830.6094f; 
+                    break;
+                case Keys.T:
+                    frequency = 880.0000f; 
+                    break;
+                case Keys.D5:
+                    frequency = 932.3275f; 
+                    break;
+                case Keys.Y:
+                    frequency = 987.7666f; 
+                    break;
+                case Keys.U:
+                    frequency = 1046.502f;
+                    break;
+                case Keys.D7:
+                    frequency = 1108.731f; 
+                    break;
+                case Keys.I:
+                    frequency = 1174.659f; 
+                    break;
+
+                case Keys.D9:
+                    frequency = 1244.508f; 
+                    break;
+                case Keys.O:
+                    frequency = 1318.510f; 
+                    break;
+                case Keys.P:
+                    frequency = 1396.913f; 
+                    break;
+                case Keys.OemMinus:
+                    frequency = 1479.978f; 
+                    break;
+                case Keys.OemOpenBrackets:
+                    frequency = 1567.982f; 
                     break;
 
             }
@@ -280,31 +372,174 @@ namespace Synthesizer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            frequency = 440.0000f;//A4
+            frequency = 220.0000f;//A2
             keySoundMs(sender,e);
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void button24_Click(object sender, EventArgs e)
         {
-            frequency = 466.1638f;//A#
+            frequency = 233.0819f;//A#2
             keySoundMs(sender, e);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            frequency = 493.8833f;//B4
+            frequency = 246.9417f;//B2
             keySoundMs(sender, e);
         }
 
+
+
+
         private void button3_Click(object sender, EventArgs e)
+        {
+            frequency = 261.6256f; //c3
+            keySoundMs(sender, e);
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            frequency = 277.1826f;//C#3
+            keySoundMs(sender, e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            frequency = 293.6648f;//D3
+            keySoundMs(sender, e);
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            frequency = 311.1270f;//D#3
+            keySoundMs(sender, e);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            frequency = 329.6276f;//E3
+            keySoundMs(sender, e);
+        }
+
+
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            frequency = 349.2282f;
+            keySoundMs(sender, e);
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            frequency = 369.9944f;
+            keySoundMs(sender, e);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            frequency = 391.9954f;
+            keySoundMs(sender, e);
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            frequency = 415.3047f;
+            keySoundMs(sender, e);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            frequency = 440.0000f;
+            keySoundMs(sender, e);
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            frequency = 466.1638f;
+            keySoundMs(sender, e);
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            frequency = 493.8833f;
+            keySoundMs(sender, e);
+        }
+
+
+
+
+
+        private void button10_Click(object sender, EventArgs e)
         {
             frequency = 523.2511f;
             keySoundMs(sender, e);
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private void button34_Click(object sender, EventArgs e)
         {
             frequency = 554.3653f;
+            keySoundMs(sender, e);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            frequency = 587.3295f;
+            keySoundMs(sender, e);
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            frequency = 622.2540f;
+            keySoundMs(sender, e);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            frequency = 659.2551f;
+            keySoundMs(sender, e);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            frequency = 698.4565f;
+            keySoundMs(sender, e);
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            frequency = 739.9888f;
+            keySoundMs(sender, e);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            frequency = 783.9909f;
+            keySoundMs(sender, e);
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            frequency = 830.6094f;
+            keySoundMs(sender, e);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            frequency = 880.0000f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            frequency = 932.3275f;
+            keySoundMs(sender, e);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            frequency = 987.7666f;
             keySoundMs(sender, e);
         }
 
@@ -313,15 +548,96 @@ namespace Synthesizer
 
 
 
+        private void button17_Click(object sender, EventArgs e)
+        {
+            frequency = 1046.502f;
+            keySoundMs(sender, e);
 
+        }
 
+        private void button39_Click(object sender, EventArgs e)
+        {
+            frequency = 1108.731f;
+            keySoundMs(sender, e);
 
+        }
 
+        private void button18_Click(object sender, EventArgs e)
+        {
+            frequency = 1174.659f;
+            keySoundMs(sender, e);
 
+        }
 
+        private void button38_Click(object sender, EventArgs e)
+        {
+            frequency = 1244.508f;
+            keySoundMs(sender, e);
 
+        }
 
+        private void button19_Click(object sender, EventArgs e)
+        {
+            frequency = 1318.510f;
+            keySoundMs(sender, e);
 
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            frequency = 1396.913f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            frequency = 1479.978f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            frequency = 1567.982f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            frequency = 1661.219f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            frequency = 1760.000f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            frequency = 1864.655f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            frequency = 1975.533f;
+            keySoundMs(sender, e);
+
+        }
+
+        private void button40_Click(object sender, EventArgs e)
+        {
+            frequency = 2093.005f;
+            keySoundMs(sender, e);
+
+        }
     }
 
 
